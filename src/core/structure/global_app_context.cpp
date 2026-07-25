5bba16101b07d6e3f707eec99636bf8690bd731f
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-07-25 13:30:20 +0800

    Update global app context

---
+++ src/core/structure/global_app_context.cpp
@@ -0,0 +1,9 @@
+#include "global_app_context.h"
+
+namespace core::structure { 
+
+    void global_app_context_init(global_app_context& ctx) {
+        
+    }
+
+}
---
+++ src/core/structure/global_app_context.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CORE_STRUCTURE_GLOBAL_APP_CONTEXT_H
+#define CORE_STRUCTURE_GLOBAL_APP_CONTEXT_H
+
+namespace core::structure {
+    
+    struct global_app_context {
+        bool init;
+    };
+
+    void global_app_context_init(global_app_context& ctx);
+
+}
+
+#endif
